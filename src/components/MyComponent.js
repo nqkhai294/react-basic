c5b743fd474f78e8be40ae13cbbaa43cc29346d7
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2024-04-23 22:43:22 +0700

    phase2

---
+++ src/components/MyComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    //JSX
+    render() {
+        return (
+            <div>
+                My First Component.
+            </div>
+        );
+    }
+}
+
+export default MyComponent;
